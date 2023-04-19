388257abc2bd2cad1a64ed35f08fb7e9cc906471
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-04-19 14:33:02 -0400

    changes to api

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-//YouTube API call
+//YouTube API call explore data explaining random method
 
-var apiKey = "AIzaSyDsBeULqXmY3viCXmBIU9KtifxDi9DnpLo";
-var YouTubeApiKey = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=searchSortUnspecified&publishedAfter=2020-01-01T00%3A00%3A00Z&q=random%20method%20java%20script%20kids&key=AIzaSyDsBeULqXmY3viCXmBIU9KtifxDi9DnpLo";
-
+var btnApiCallEl = $("#call-api");
 
 
-function YouTubeApiCall(YouTubeApiKey) {
-    fetch(YouTubeApiKey)
+function youTubeApiCall() {
+
+    var youTubeApiKey = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTNXca5Zhdc0uoAHGMGOEiw&key=AIzaSyBivby0lJQWzk_xXjxxkxUjgMlfy65cbbM";
+
+    fetch(youTubeApiKey)
     .then (function (response) {
         if (response.ok){
             response.json()
             .then (function(data){
                 console.log(data);
+                // DisplayRandomMethodeVideo(data);
             })
         }
         else {
@@ -20,10 +22,19 @@ function YouTubeApiCall(YouTubeApiKey) {
     })
     }
 
-
-
-    YouTubeApiCall(YouTubeApiKey);
+    btnApiCallEl.on("click", function(){
+        youTubeApiCall();
+    });
     
+
+    // function DisplayRandomMethodeVideo (data){
+    //         $("<iframe>").attr("class", "youtube-player video-container").attr("id","player").attr("type", "text/html").attr("src", "http://www.youtube.com/embed/" + i);
+    // }
+
+
+    
+
+//YouTube API call exploring data regarding css color and background color choice    
     
 
 // background color generator component
@@ -32,13 +43,9 @@ var colorGeneratorContainer = $("#color-generator-container");
 var switchBtnEl = $("#switch-color-btn");
 function randomBackground(){
     var r = Math.ceil(Math.random() * 255);
-    console.log(r);
     var g = Math.ceil(Math.random() * 255);
-    console.log(g);
     var b = Math.ceil(Math.random() * 255);
-    console.log(b);
     var opacity = (Math.random() * 1).toFixed(1);
-    console.log(opacity);
     
     colorGeneratorContainer.attr("style","background-color: rgba(" + r + "," + g + "," + b + "," + opacity +")");
     
@@ -48,8 +55,6 @@ switchBtnEl.on("click",function(event){
     event.preventDefault();
     randomBackground()});
     
-    
-YouTubeApiCall(YouTubeApiKey);
 
 // daily inspirational quotes API
 const options = {
