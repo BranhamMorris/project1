672f0b7ae1439b751a0ee563301ac7f440750195
Corey Vesey <vesey8507@gmail.com>
2023-04-24 06:46:45 -0700

    put inf memory game feature

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # project1
 
-coding website
+Description
+
+This is an interactive coding app targeting users who are new to coding.
+
+This webpage includes the following:
+
+- The app contains a header and a footer.
+- The app contains two server side APIs.
+- The app uses CSS Bootstrap framework.
+- The webpages contains three sections: vision, study-tool, and resources.
+- The study-tool contains two interactive activities that the user can explore.
+- When the user clicks on one of the link in the navigation bar then the UI scrolls to the appropriate section.
+
+Links
+
+[URL of the deployed application](https://lizas2022.github.io/project1/).
+
+[URL of the github repository](https://github.com/LizaS2022/project1.git).
+
+Usage
+
+The webpage is optimized and can be viewed on various screens and devices.
+
+![1682083645845](image/README/1682083645845.png)
--- js/script.js
+++ js/script.js
@@ -1,28 +1,140 @@
-//inserting a YouTube video to the webpage
+//YouTube API call explore data explaining random method
 
-var YouTubeApiKey = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCOxqgCwgOqC2lMqC5PYz_Dg&key=AIzaSyDsBeULqXmY3viCXmBIU9KtifxDi9DnpLo";
-var youTubecontainer = $("#youtube-video-container");
-
+var eduVideosContainer = $("#edu-coding-topics").attr("class" , "col p-12 g-4 py-5 row-col-1 row-col-md-2");
 
 
-function YouTubeApiCall(YouTubeApiKey) {
-fetch(YouTubeApiKey)
+function youTubeApiCall() {
+
+
+    var youTubeApiKey = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTNXca5Zhdc0uoAHGMGOEiw&maxResults=40&key=AIzaSyBmXIYgbfQymw5bPQbNaSaPkJucq1Ld5Do";
+
+
+    fetch(youTubeApiKey)
+    .then (function (response) {
+        if (response.ok){
+            response.json()
+            .then (function(data){
+                displayEduVideos(data);
+            })
+        }
+
+        else {
+            alert("404 Error: could not find");
+        }
+    })
+    }
+
+
+    function displayEduVideos(data) {
+        for (var  i=0; i<data.items.length; i++) {
+        var title = data.items[i].snippet.title;
+        console.log(title);
+        var videoId = data.items[i].id.videoId;
+            if (title == "Creating Random Numbers in JavaScript" || title == "The Anatomy Of HTML Tags" || title == "Learn Headings in HTML" || title == "Understanding Modulo In JavaScript | %"){
+                var iframeEl = $("<iframe>").attr("class", "youtube-player video-container edu-videos").attr("id","player").attr("type", "text/html").attr("style", "width:100% height:150px").css("margin","4px").attr("src", "http://www.youtube.com/embed/" + videoId);
+                eduVideosContainer.append(iframeEl);
+                var resourcesEl = $("#resources").attr("class", "container");
+                resourcesEl.append(eduVideosContainer);
+                }
+            }}
+            youTubeApiCall()
+
+
+// background color generator component
+
+var colorGeneratorContainer = $("#color-generator-container");
+var switchBtnEl = $("#switch-color-btn");
+function randomBackground(){
+    var r = Math.ceil(Math.random() * 255);
+    var g = Math.ceil(Math.random() * 255);
+    var b = Math.ceil(Math.random() * 255);
+    var opacity = (Math.random() * 1).toFixed(1);
+    
+    colorGeneratorContainer.attr("style","background-color: rgba(" + r + "," + g + "," + b + "," + opacity +")");
+    
+};
+
+switchBtnEl.on("click",function(event){
+    event.preventDefault();
+    randomBackground()});
+    
+
+
+
+//motivational quotes API
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d368c3e96mshe093caf1191f67bp18aea8jsn204c186e106d',
+		'X-RapidAPI-Host': 'motivational-quotes-quotable-api.p.rapidapi.com'
+	}
+};
+
+fetch('https://motivational-quotes-quotable-api.p.rapidapi.com/motivational_quotes', options)
 .then (function (response) {
-    console.log(response);
     if (response.ok){
         response.json()
         .then (function(data){
             console.log(data);
-            return data;
-        
-            
+            var quote = data.quote
+            var quoteEl = $("#quote").text(quote);
         })
-
     }
+
     else {
         alert("404 Error: could not find");
     }
 })
-}
 
-YouTubeApiCall(YouTubeApiKey);
+//Memory Card Game---------------------------------------------------------------------------------//
+
+    //Card Flip Function//
+
+    //Making list of all cards and storing the value//
+    var cards = document.querySelectorAll(".card")                  
+    //determining if it is the first or second click for the pairing//
+    var flippedCard = false;                                        
+    var firstCard, secondCard;   
+    //locks the board so only a pair of cards can be selected//                                   
+    var locked = false;                                             
+    
+    //Assigning how the function of flipping a card will work//
+    function flipCard() {
+        if (locked) return;
+        //add flip to every card class with "this"//
+        this.classList.add("flip");  
+        //statement for first click, sets firstCard to card class(this)//                               
+        if (!flippedCard) {                                          
+            flippedCard = true;
+            firstCard = this;
+        //second click//
+        } else {
+            flippedCard = false;
+            secondCard = this;
+            //Matching cards, gave each desired pair of card class a data id, if when 2 cards are clicked and they have same value then they are removed from list(removeEventListener)//
+            if (firstCard.dataset.id === secondCard.dataset.id) {
+                firstCard.removeEventListener("click", flipCard);
+                secondCard.removeEventListener("click", flipCard);
+            //If its not a match then the cards are flipped back over by removing the flip with (removeEventListener)//
+            } else {
+                locked = true;
+                setTimeout(() => {
+                    firstCard.classList.remove("flip");
+                    secondCard.classList.remove("flip");
+                    locked = false;
+                }, 2000)
+                
+            }
+            
+        }
+    }
+    //Function mix the cards up as someone enters the site using IIFE immediately invoked function expression//
+    (function mixing() {
+        cards.forEach(card => {
+            var assigningCards = Math.floor(Math.random() * 16);
+            card.style.order = assigningCards;
+        });
+    })();
+    
+    //looping through each card(addEventListener) looking a click and when that happens it will flip the card//
+    cards.forEach(card => card.addEventListener("click", flipCard));  
